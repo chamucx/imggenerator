4439c3a5d1137566fa273d9b02ec16090457cce7
Antonio <joseantoniosysnet@outlook.com>
2024-09-16 04:14:48 -0500

    added images

--- app.py
+++ app.py
@@ -1,38 +1,44 @@
 from flask import Flask, render_template, request
+import os
 from openai import AzureOpenAI
+import json
 import httpx
+
 
 app = Flask(__name__)
 
+# Creando el cliente de Azure
 client = AzureOpenAI(
     api_version="2024-05-01-preview",
-    azure_endpoint="https://antoniox.openai.azure.com/",
-    api_key="18db2af83abc498b92d10bf0397fbdda",
+    azure_endpoint="",
+    api_key="",
 )
 
-@app.route("/", methods=["GET", "POST"])
+
+@app.route('/', methods=["GET", "POST"])
 def index():
+
+    # Creando el app de openAI
     image_url = None
+
     if request.method == "POST":
-        # Obtener los datos del formulario
         object_to_generate = request.form["object_to_generate"]
-        time_of_day = request.form["time_of_day"]
+        time_of_the_day = request.form["time_of_the_day"]
         additional_feature = request.form.get("additional_feature", "")
 
-        # Crear el prompt dinámico
-        prompt = f"Generate an image of {object_to_generate} {time_of_day}, {additional_feature}."
+        prompt = f"Generate an image of {object_to_generate} {
+            time_of_the_day} , {additional_feature}"
 
-        # Generar la imagen
         result = client.images.generate(
-            model="deploymentmodel",  # nombre de tu modelo de despliegue
+            model="Dalle3",  # the name of your DALL-E 3 deployment
             prompt=prompt,
             n=1
         )
 
-        # Obtener la URL de la imagen generada
-        image_url = result.data[0].url  
+        image_url = result.data[0].url
 
     return render_template("index.html", image_url=image_url)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
